308ab87bec03bc7efcf22e09c4952ea3209f5b14
elinah <elinah254.mmboneelinah@gmail.com>
2023-10-16 21:16:07 +0300

    javascript

---
+++ test.js
@@ -0,0 +1,79 @@
+ 
+ //global variables
+ 
+  console.log ("43")
+  alert ("success")
+  document.write ("hello")
+
+  //data types
+  //numbers
+let a=7;
+let b=5;
+
+
+  //integers
+  let num=4.1;
+  //string
+  let student="sarah";
+
+  //array
+  let names= ["nine","two","one"]
+
+  //javascript object
+  
+ let person = [
+  surname="cherop",
+  school="Moi",
+  age= 21
+ ]
+
+console.log( person.school)
+console.log( person.surname)
+console.log(person.age)
+
+//javasript arithmetic
+
+console.log(30/5);
+console.log(20+5);
+console.log(5*4);
+console.log(20-2);
+
+
+// //variables
+// const 
+// var
+// let 
+
+//javascript addition of string
+let z= "4" +4;
+let r= "miriam" + 7;
+let h= 5;
+console.log(r)
+console.log(z)
+
+
+
+
+
+
+//conditional statement
+
+
+
+let j=10
+
+ if (j=10) {console.log ("positive")
+  
+ } else {console.log (" negative")
+  
+ } 
+
+
+ function myFunction(num1,num2) {
+  // console.log(num1+num2) 
+  return num1 + num2
+ }
+
+ 
+ let  result =myFunction(4,5)
+ console.log (result)
